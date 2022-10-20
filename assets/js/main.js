08e926a5894c79226a094edb03afa7ee5937dadc
sweet202020 <dolcealessio1@gmail.com>
2022-10-20 17:24:12 +0200

    fix object properties

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,39 +6,39 @@ BONUS 2: Organizzare i singoli membri in card/schede
 //MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
 const team = [
     {
-        Name: 'Wayne Barnett',
-        Role: 'Founder & CEO',
-        Image: 'wayne-barnett-founder-ceo.jpg'
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
         
     },
     {
-        Name: 'Angela',
-        Role: 'Chief Editor',
-        Image: 'angela-caroll-chief-editor.jpg'
+        name: 'Angela',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
         
     },
     {
-        Name: 'Waleter gordon',
-        Role: 'Chief Editor',
-        Image: 'walter-gordon-office-manager.jpg'
+        name: 'Waleter gordon',
+        role: 'Chief Editor',
+        image: 'walter-gordon-office-manager.jpg'
         
     },
     {
-        Name: 'Angela Lopez',
-        Role: 'Social Media Manager',
-        Image: 'angela-lopez-social-media-manager.jpg'
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
         
     },
     {
-        Name: 'Scott Estrada',
-        Role: 'Developer',
-        Image: 'scott-estrada-developer.jpg'
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
         
     },
     {
-        Name: 'Barbara Ramos',
-        Role: 'Grafic designer',
-        Image: 'barbara-ramos-graphic-designer.jpg'
+        name: 'Barbara Ramos',
+        role: 'Grafic designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
         
     },
 ]
@@ -52,7 +52,7 @@ for (let i = 0; i < team.length; i++) {
     const infoPerson = team[i];
     console.log(infoPerson);
     //creo una variabile e con i backtip aggiungo i miei div con la classe col-2
-    const colEL=`<div class="col-4 card my-3 text-center"><img src="./assets/img/${infoPerson.Image}" alt=""><h3>${infoPerson.Name}</h3><p>${infoPerson.Role}</p></div>`;
+    const colEL=`<div class="col-4 card my-3 text-center"><img src="./assets/img/${infoPerson.image}" alt=""><h3>${infoPerson.name}</h3><p>${infoPerson.role}</p></div>`;
     //inserisco il tutto con insertadjacenthtml
     rowEl.insertAdjacentHTML('beforeend', colEL)
 
